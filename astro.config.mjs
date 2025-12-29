85ab033013075755fbbed1be236a1b30c1e5d661
amerfathullah <amer@tfx.global>
2025-12-29 17:58:11 +0800

    configure sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,26 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import sitemap from '@astrojs/sitemap';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.dev.amerfathullah.com",
+  // Enables HTML minification
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto' // Helps reduce render-blocking CSS
+  },
   vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
     plugins: [tailwindcss()],
   },
-  // Add your domain name here
-  site: "https://yourwebsite.com/",
-  integrations: [ sitemap()]
+  integrations: [sitemap()]
 });
